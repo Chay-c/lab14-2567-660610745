63e8c46b89a8da3bcaf5a55bc38f29c09a5a9732
Chay <chayangkul_c@cmu.ac.th>
2024-09-07 16:17:17 +0700

    atedup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Title, Pagination, Group } from "@mantine/core";
+import YourRating from "@components/YourRating";
+import YourReview from "@components/YourReview";
+import CommentReview from "@components/CommentReview";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <Container size="600px">
-      <Title order={2}>Food Review 🍕</Title>
-
-      <Text ta="center" my="sm">
-        Copyright © 2024 Chayanin Suatap 650610560
-      </Text>
+      <Title order={2}>
+        Food Review 🍕
+      </Title>
+    <YourRating />
+    <YourReview />
+    <CommentReview name="Elon Musk" rating={5} review="Best pizza in this world. I give you X score." />
+    <CommentReview name="Mark Zuck" rating={4} review="My favourite part is pepperoni" />
+      <Group justify="center">
+        <Pagination mt="md" total={20} color="orange" />
+      </Group>
+    <Footer year={2024} name="Chayangkul Chanjarupong" studentId="660610745" />
     </Container>
   );
 }
